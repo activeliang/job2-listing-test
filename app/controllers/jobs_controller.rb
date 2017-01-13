b7954c5b93e9b7c6858d2c70d2a993ef4019334e
activeliang <liang@activeliangdeiMac.local>
2017-01-13 21:45:17 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -44,8 +44,10 @@ end
       redirect_to jobs_path
   end
 
+private
 
-
-
+def job_params
+  params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
+end
 
 end
